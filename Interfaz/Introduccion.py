cfea018f4469d977b189110e33d3334aa911b2ff
LetyCastineyra <a01173881@tec.mx>
2023-11-02 16:01:00 -0600

    uno

--- Interfaz/Introduccion.py
+++ Interfaz/Introduccion.py
@@ -1,1 +1,17 @@
 print('hola')
+
+print('holaaa, bienvenidos')
+
+a= 50
+b= 56
+c= 89
+
+def suma():
+    print(a+b+c)
+    
+suma()
+
+def imprime():
+    print("Esta es la rama para desarrollar el backend")
+imprime()
+
